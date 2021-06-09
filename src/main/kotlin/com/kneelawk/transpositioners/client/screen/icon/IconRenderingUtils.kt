a0a2ca255edd1698176dbe7bc2897d7fffa764b8
Kneelawk <kneelawk@gmail.com>
2021-06-09 13:14:53 -0700

    Fix transparency in gui icons

--- src/main/kotlin/com/kneelawk/transpositioners/client/screen/icon/IconRenderingUtils.kt
+++ src/main/kotlin/com/kneelawk/transpositioners/client/screen/icon/IconRenderingUtils.kt
@@ -4,11 +4,11 @@ import com.kneelawk.transpositioners.TPConstants.str
 import com.kneelawk.transpositioners.mixin.api.RenderLayerHelper
 import com.mojang.blaze3d.platform.GlStateManager
 import com.mojang.blaze3d.systems.RenderSystem
-import net.minecraft.client.MinecraftClient
+import io.github.cottonmc.cotton.gui.widget.data.Texture
 import net.minecraft.client.render.*
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.util.Identifier
 import net.minecraft.util.Util
-import net.minecraft.util.math.Matrix4f
 
 object IconRenderingUtils {
     private val ICON_TRANSPARENCY = RenderPhase.Transparency(str("icon_transparency"),
@@ -39,9 +39,28 @@ object IconRenderingUtils {
     }
 
     fun texturedRect(
-        model: Matrix4f, consumers: VertexConsumerProvider, x: Int, y: Int, width: Int, height: Int,
+        matrices: MatrixStack,
+        consumers: VertexConsumerProvider,
+        x: Int,
+        y: Int,
+        width: Int,
+        height: Int,
+        texture: Texture,
+        color: Int,
+        opacity: Float
+    ) {
+        texturedRect(
+            matrices, consumers, x, y, width, height, texture.image, texture.u1, texture.v1, texture.u2, texture.v2,
+            color, opacity
+        )
+    }
+
+    fun texturedRect(
+        stack: MatrixStack, consumers: VertexConsumerProvider, x: Int, y: Int, width: Int, height: Int,
         texture: Identifier, u1: Float, v1: Float, u2: Float, v2: Float, color: Int, opacity: Float
     ) {
+        val model = stack.peek().model
+
         var adjWidth = width
         var adjHeight = height
 
@@ -57,5 +76,49 @@ object IconRenderingUtils {
             .texture(u2, v2).next()
         layer.vertex(model, (x + adjWidth).toFloat(), y.toFloat(), 0.0f).color(r, g, b, opacity).texture(u2, v1).next()
         layer.vertex(model, x.toFloat(), y.toFloat(), 0.0f).color(r, g, b, opacity).texture(u1, v1).next()
+    }
+
+    fun texturedRect(
+        matrices: MatrixStack, x: Int, y: Int, width: Int, height: Int, texture: Texture, color: Int, opacity: Float
+    ) {
+        texturedRect(
+            matrices, x, y, width, height, texture.image(), texture.u1(), texture.v1(), texture.u2(), texture.v2(),
+            color, opacity
+        )
+    }
+
+    fun texturedRect(
+        matrices: MatrixStack, x: Int, y: Int, width: Int, height: Int, texture: Identifier, u1: Float, v1: Float,
+        u2: Float, v2: Float, color: Int, opacity: Float
+    ) {
+        val model = matrices.peek().model
+
+        var adjWidth = width
+        var adjHeight = height
+
+        if (adjWidth <= 0) adjWidth = 1
+        if (adjHeight <= 0) adjHeight = 1
+        val r = (color shr 16 and 255) / 255.0f
+        val g = (color shr 8 and 255) / 255.0f
+        val b = (color and 255) / 255.0f
+        val tessellator = Tessellator.getInstance()
+        val buffer = tessellator.buffer
+        RenderSystem.setShaderTexture(0, texture)
+        RenderSystem.setShaderColor(r, g, b, opacity)
+        RenderSystem.enableBlend()
+        RenderSystem.blendFuncSeparate(
+            GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA,
+            GlStateManager.SrcFactor.ONE, GlStateManager.DstFactor.ZERO
+        )
+        RenderSystem.setShader(GameRenderer::getPositionTexShader)
+        buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE)
+        buffer.vertex(model, x.toFloat(), (y + adjHeight).toFloat(), 0f).texture(u1, v2).next()
+        buffer.vertex(model, (x + adjWidth).toFloat(), (y + adjHeight).toFloat(), 0f).texture(u2, v2).next()
+        buffer.vertex(model, (x + adjWidth).toFloat(), y.toFloat(), 0f).texture(u2, v1).next()
+        buffer.vertex(model, x.toFloat(), y.toFloat(), 0f).texture(u1, v1).next()
+        buffer.end()
+        BufferRenderer.draw(buffer)
+        RenderSystem.disableBlend()
+        RenderSystem.defaultBlendFunc()
     }
 }
